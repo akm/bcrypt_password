364ae64b9e9efe6644f3375a61ee36f742ed5b0f
akm <akm2000@gmail.com>
2023-10-14 20:20:10 +0900

    :sparkles: Implement digest sub command

--- cmd/bcryptpw/digest.go
+++ cmd/bcryptpw/digest.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func Digest() *cobra.Command {
@@ -10,7 +13,18 @@ func Digest() *cobra.Command {
 		Use:     "digest",
 		Short:   "digest PASSWORD",
 		Example: "bcryptpw digest YOUR_PASSWORD",
+		Args: func(cmd *cobra.Command, args []string) error {
+			if len(args) < 1 {
+				return fmt.Errorf("requires at least 1 arg(s), only received %d", len(args))
+			}
+			return nil
+		},
 		RunE: func(cmd *cobra.Command, args []string) error {
+			hashed, err := bcrypt.GenerateFromPassword([]byte(args[0]), cost)
+			if err != nil {
+				return err
+			}
+			fmt.Println(string(hashed))
 			return nil
 		},
 	}
